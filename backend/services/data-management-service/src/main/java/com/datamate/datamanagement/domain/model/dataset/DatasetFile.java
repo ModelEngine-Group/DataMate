98b81a4975cf3d949899dff8c6e16b56d1373089
uname <2986773479@qq.com>
2025-10-29 16:26:46 +0800

    feature: 查询数据集详情时能够获取到文件标签分布

--- backend/services/data-management-service/src/main/java/com/datamate/datamanagement/domain/model/dataset/DatasetFile.java
+++ backend/services/data-management-service/src/main/java/com/datamate/datamanagement/domain/model/dataset/DatasetFile.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -25,11 +26,20 @@ public class DatasetFile {
     private String fileType; // JPG/PNG/DCM/TXT
     private Long fileSize; // bytes
     private String checkSum;
-    private List<String> tags;
+    private String tags;
     private String metadata;
     private String status; // UPLOADED, PROCESSING, COMPLETED, ERROR
     private LocalDateTime uploadTime;
     private LocalDateTime lastAccessTime;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    /**
+     * 解析标签
+     *
+     * @return 标签列表
+     */
+    public List<String> analyzeTag() {
+        return Arrays.asList(tags.split(","));
+    }
 }
