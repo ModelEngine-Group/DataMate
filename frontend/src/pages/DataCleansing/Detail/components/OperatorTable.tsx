a983610c61b739ad508d9ad78f40d5d3de257c91
hhhhsc <1710496817@qq.com>
2025-11-12 09:46:11 +0800

    fix: 算子列表可点击

--- frontend/src/pages/DataCleansing/Detail/components/OperatorTable.tsx
+++ frontend/src/pages/DataCleansing/Detail/components/OperatorTable.tsx
@@ -1,17 +1,23 @@
-import {Steps} from "antd";
+import {Steps, Typography} from "antd";
 import {CleansingTask} from "@/pages/DataCleansing/cleansing.model.ts";
+import {useNavigate} from "react-router";
 
 export default function OperatorTable({ task }: { task: CleansingTask }) {
+  const navigate = useNavigate();
 
   return (
     <>
         <Steps
           progressDot
-          current={task?.instance.length}
           direction="vertical"
           items={Object.values(task?.instance).map((item) => ({
-            title: item.name,
-            description: item.description
+            title: <Typography.Link
+              onClick={() => navigate(`/data/operator-market/plugin-detail/${item.id}`)}
+            >
+              {item.name}
+            </Typography.Link>,
+            description: item.description,
+            status: "finish"
           }))}
           className="overflow-auto"
         />
