2b97a8c0ae9be28091388364bf7da492f9656803
uname <2986773479@qq.com>
2025-12-29 22:03:49 +0800

    fix: remove terrabase dependency

--- backend/api-gateway/src/main/java/com/datamate/gateway/ApiGatewayApplication.java
+++ backend/api-gateway/src/main/java/com/datamate/gateway/ApiGatewayApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * API Gateway & Auth Service Application
@@ -14,8 +12,6 @@ import org.springframework.context.annotation.ComponentScan;
  * 提供路由、鉴权、限流等功能
  */
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.datamate.gateway", "com.terrabase"})
-@EnableFeignClients(basePackages = {"com.terrabase"})
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
