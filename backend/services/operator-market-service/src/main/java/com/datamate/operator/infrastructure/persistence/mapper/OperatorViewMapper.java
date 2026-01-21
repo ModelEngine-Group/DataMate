94d732cd55202cd2c70006f642bd0339836696f5
uname <2986773479@qq.com>
2026-01-21 09:53:17 +0800

    feature: realize that data sets, data cleaning, and knowledge bases are isolated according to the creator, and operators are not isolated.

--- backend/services/operator-market-service/src/main/java/com/datamate/operator/infrastructure/persistence/mapper/OperatorViewMapper.java
+++ backend/services/operator-market-service/src/main/java/com/datamate/operator/infrastructure/persistence/mapper/OperatorViewMapper.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.datamate.common.infrastructure.config.IgnoreDataScopeAnnotation;
 import com.datamate.operator.domain.model.OperatorView;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -11,6 +12,7 @@ import org.apache.ibatis.annotations.Select;
 
 
 @Mapper
+@IgnoreDataScopeAnnotation
 public interface OperatorViewMapper extends BaseMapper<OperatorView> {
     @Select("SELECT operator_id AS id, operator_name AS name, description, version, inputs, outputs, runtime, " +
         "settings, is_star, created_at, updated_at, " +
