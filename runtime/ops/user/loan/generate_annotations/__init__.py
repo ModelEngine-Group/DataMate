97b9e84352fde008dce32a2e6df02525bbec7d83
hhhhsc <1710496817@qq.com>
2026-01-28 10:44:22 +0800

    更新包名

--- runtime/ops/user/loan/generate_annotations/__init__.py
+++ runtime/ops/user/loan/generate_annotations/__init__.py
@@ -2,8 +2,8 @@
 from datamate.core.base_op import OPERATORS
 
 # 注册算子
-# 文件夹名必须是 annotation_gen_op
+# 文件夹名必须是 generate_annotations
 OPERATORS.register_module(
     module_name='AnnotationGenOperator', 
-    module_path="ops.user.annotation_gen_op.process"
+    module_path="ops.user.generate_annotations.process"
 )
--- runtime/ops/user/loan/generate_data/__init__.py
+++ runtime/ops/user/loan/generate_data/__init__.py
@@ -3,8 +3,8 @@ from datamate.core.base_op import OPERATORS
 
 # 注册算子
 # 注意：module_name 必须与 metadata.yml 中的 raw_id 一致
-# module_path 中的 'data_gen_op' 必须与文件夹名称一致
+# module_path 中的 'generate_data' 必须与文件夹名称一致
 OPERATORS.register_module(
     module_name='DataGenOperator', 
-    module_path="ops.user.data_gen_op.process"
+    module_path="ops.user.generate_data.process"
 )
