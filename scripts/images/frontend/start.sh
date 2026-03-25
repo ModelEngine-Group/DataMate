432b07ce1a078db55101135230090e2e4cbd2102
MoeexT <yuwancumian666@outlook.com>
2026-03-25 15:00:43 +0800

    feat: add switch to enable real-ip forwarding

--- scripts/images/frontend/start.sh
+++ scripts/images/frontend/start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+function set_proxy_protocol() {
+    if [ "${REAL_IP_MODE}" != "proxy_protocol" ]; then
+        echo "REAL_IP_MODE is ${REAL_IP_MODE}, no need to update nginx configuration file."
+        return 0
+    fi
+    sed -i 's/listen       3000;/listen       3000 proxy_protocol;/' /opt/frontend/http_backend.conf
+    sed -i 's/listen       3000;/listen       3000 proxy_protocol;/' /opt/frontend/https_backend.conf
+    sed -i '/access_log.*main/a\    set_real_ip_from 0.0.0.0/0;' /opt/frontend/nginx.conf
+    sed -i '/access_log.*main/a\    real_ip_header proxy_protocol;' /opt/frontend/nginx.conf
+    echo "ginx configuration file updated."
+}
+
 if [ -f "/cert/server.pem" ]; then
     cp /cert/server.pem /etc/nginx/cert/server.pem
     chown nginx:nginx /etc/nginx/cert/server.pem
@@ -18,6 +30,8 @@ if [ -f "/cert/server.key" ]; then
     chown nginx:nginx /etc/nginx/cert/server.key
 fi
 
+set_proxy_protocol
+cp /opt/frontend/nginx.conf /etc/nginx/nginx.conf
 if [ -f "/etc/nginx/cert/server.pem" ]; then
     cp /opt/frontend/https_backend.conf /etc/nginx/conf.d/default.conf
     cp /opt/frontend/routes.inc /etc/nginx/conf.d/routes.inc
